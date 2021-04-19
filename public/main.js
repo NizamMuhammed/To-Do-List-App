ffb5ee98fbcaa187e82cca282eb2abe456a8bb86
NizamMuhammed <mohammednizampn@gmail.com>
2021-04-19 23:46:50 +0530

    All Files deleted for new ones

--- public/main.js
+++
@@ -1,8 +0,0 @@
-var update = document.querySelector('#update-button');
-
-update.addEventListener('click', function (_) {
-    fetch('/update', {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json' }
-    });
-});
