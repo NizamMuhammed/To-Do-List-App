f9b210640f2912188e9b5070f96fdd9c2144e3d4
NizamMuhammed <mohammednizampn@gmail.com>
2021-05-09 21:39:55 +0530

    Changed .env file of client

---
+++ client/src/testData.ts
@@ -0,0 +1,6 @@
+export const data = {
+  todos: [
+    { _id: "6096a708866f9cbddc43c5ee", title: "Buy milk", status: false },
+    { _id: "6096bfcbd8280a05230bfc6b", title: "Buy Paper", status: true },
+  ],
+};
