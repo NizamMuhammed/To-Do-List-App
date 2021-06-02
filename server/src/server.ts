db9e2ede398e3b31fbf777088f841b4e23fef168
NizamMuhammed <mohammednizampn@gmail.com>
2021-06-03 02:27:46 +0530

    Added comments to server files

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,17 @@ import connect from "./db/connection";
 import router from "./routes/home";
 import cors from "cors";
 
+/**
+ * To get all the values from the .env file
+ */
 dotenv.config();
+/**
+ **Create a new Express App
+ */
 const app: Express = express();
+/**
+ ** Use CORS to give access to requests from multiple origins, like different IPs/localhosts
+ */
 app.use(cors());
 app.use(express.json());
 app.use(
